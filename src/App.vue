44ca8ded299a21456d2ec566a0fca81f468d16e9
Jason Dees <jhdees@gmail.com>
2021-01-30 10:06:43 -0500

    updated some functionality to get new harold images

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <HaroldContainer />
+  </div>
+</template>
+
+<script>
+import HaroldContainer from './components/HaroldContainer.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HaroldContainer
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HaroldContainer.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="container">
+        <div class="left-area" @click="nextHarold">left</div>
+        <!-- this can be the harold stack -->
+        <img v-bind:src="haroldUrl" />
+        <div class="right-area" @click="nextHarold">right</div>
+        how can i do like a psuedo stack of harolds?
+        Should I just do a create/destroy thing like usual?
+        So once the image gets to the edge of the screen instead of destroying 
+        i put it on the back and call get a new harold again.
+        I think create/destroy would just be easier
+
+    </div>
+</template>
+<script>
+export default {
+  name: 'HaroldContainer',
+  props: {
+
+  },
+  data: function() {
+    return {
+        haroldUrl: 'https://stuffonharold-node.vercel.app/api/image'
+    }
+  },
+  methods: {
+      nextHarold: function() {
+          this.haroldUrl = 'https://stuffonharold-node.vercel.app/api/image?'+Date();
+      }
+  }
+}
+</script>
+<style scoped>
+    .container {
+        display: grid;
+        background-color:green;
+        grid-template-columns: 2fr 3fr 2fr;
+        grid-template-rows: 1fr;
+    }
+    .left-area {
+        background-color:orange;
+    }
+    .right-area {
+        background-color:purple;
+    }
+</style>
